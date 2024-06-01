ecde350d0af18737742b43cd9c632a93a5ff4ae8
Iarlen <iarlensr@hotmail.com>
2024-06-01 18:32:59 -0300

    feat: created TotalInvoice component.

---
+++ components/TotalInvoice.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { defineProps } from 'vue'
+
+  defineProps({
+    total: {
+      type: Number,
+      default: 0
+    },
+    title: {
+      type: String,
+    },
+    isCurrent: {
+      type: Boolean,
+      default: false
+    }
+  })
+</script>
+
+<template>
+  <div class="flex flex-col gap-1 p-3 border border-neutral-600/60 rounded w-fit">
+    <p class="text-sm tracking-wider">{{ title }}</p>
+    <span 
+    class="text-xl font-semibold"
+    :class="{ 'text-red-500': isCurrent }"
+    >{{ formatPrice(total) }}</span>
+  </div>
+</template>
