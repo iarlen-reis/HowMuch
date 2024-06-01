c1290b2a88a34eae379a73f2dc65dfa09fce0c70
Iarlen <iarlensr@hotmail.com>
2024-06-01 02:13:53 -0300

    feat: created footer component.

---
+++ components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="container px-2 py-6 mx-auto flex items-center justify-center lg:justify-end">
+    <a
+      href="https://iarlenreis.com.br"
+      target="_blank"
+      class="text-sm text-black/80 lg:text-lg hover:opacity-85 transition-opacity"
+      >© 2024 - Iarlen Reis</a
+    >
+  </footer>
+</template>
