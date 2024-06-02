c8ac912c65da70ddd396a6de7659d01c0dda6c3e
Iarlen <iarlensr@hotmail.com>
2024-06-02 00:36:06 -0300

    feat: created Navigation component.

---
+++ components/Navigation.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface NavigationProps {
+  title: string;
+  url: string;
+}
+
+const props = defineProps<NavigationProps>();
+</script>
+
+<template>
+  <NuxtLink
+    :to="props.url"
+    class="flex items-center gap-2 hover:opacity-85 transition-opacity"
+  >
+    <div
+      class="flex items-center justify-center size-8 rounded-full border border-zinc-400"
+    >
+      <Icon name="ic:outline-arrow-back-ios" class="text-base" />
+    </div>
+    <span class="text-base">{{ props.title }}</span>
+  </NuxtLink>
+</template>
