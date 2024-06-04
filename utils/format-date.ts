6e38cb72c552df170bee57173667428692075c1e
Iarlen <iarlensr@hotmail.com>
2024-06-04 02:38:28 -0300

    refactor: modifying the useful function of formatting date.

--- utils/format-date.ts
+++ utils/format-date.ts
@@ -1,6 +1,7 @@
-import { format } from 'date-fns'
-import { ptBR } from 'date-fns/locale'
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function (date: string): string {
-  return format(new Date(date), "dd 'de' MMMM, yyyy", { locale: ptBR })
+  const dateString = new Date(date);
+  return format(dateString, "dd 'de' MMM, yyyy", { locale: ptBR });
 }
