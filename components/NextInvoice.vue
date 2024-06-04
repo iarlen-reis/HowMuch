8fe0f3ef11df624bfd4df3eb44e45f5a0ef6ee47
Iarlen <iarlensr@hotmail.com>
2024-06-04 02:37:13 -0300

    feat: creating next invoices component.

---
+++ components/NextInvoice.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface InvoiceProps {
+  id: number;
+  date: string;
+  amount: number;
+}
+
+const props = defineProps<InvoiceProps>();
+</script>
+
+<template>
+  <li>
+    <NuxtLink
+      to="/invoices"
+      class="flex p-3 px-0 md:px-3 items-center gap-2 justify-between text-base md:text-lg xl:text-xl rounded hover:bg-neutral-600/10 transition-colors"
+    >
+      <span
+        >{{ formatDate(props.date) }} - R$ {{ formatPrice(props.amount) }}</span
+      >
+      <Icon name="ic:baseline-chevron-right" font-size="24" />
+    </NuxtLink>
+  </li>
+</template>
