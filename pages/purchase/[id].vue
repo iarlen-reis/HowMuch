faac672923123d1f8d37aa4408611c60b65fe2d0
Iarlen <iarlensr@hotmail.com>
2024-06-09 23:52:31 -0300

    feat: integrating the purchase by id page with the api using actions.

--- pages/purchase/[id].vue
+++ pages/purchase/[id].vue
@@ -1,40 +1,45 @@
 <script setup lang="ts">
+import actions from "~/actions";
+
 definePageMeta({
   layout: "custom",
 });
 
 const route = useRoute();
-const { id } = route.params;
+const { id } = route.params as { id: string };
+
+const purchase = await actions.purchase.purchaseById(id);
 </script>
 
 <template>
   <div class="flex flex-col gap-4">
     <Navigation url="/" title="Compra na shopee" />
-    <div class="flex flex-col gap-1 mt-2">
-      <h1 class="text-2xl line-clamp-1 lg:text-3xl">Compra na shopee</h1>
-      <p class="text-sm text-zinc-500 lg:text-base">11 de maio, 2024</p>
+    <div class="flex flex-col gap-1 mt-2" v-if="purchase?.title">
+      <h1 class="text-2xl line-clamp-1 lg:text-3xl">{{ purchase.title }}</h1>
+      <p class="text-sm text-zinc-500 lg:text-base">
+        {{ formatDate(purchase.date) }}
+      </p>
     </div>
-    <div class="flex flex-col gap-1">
+    <div class="flex flex-col gap-1" v-if="purchase?.value">
       <h2 class="text-lg lg:text-xl">Valor da compra</h2>
       <span class="text-xl text-zinc-500 lg:text-2xl"
-        >R$ {{ formatPrice(120) }}</span
+        >R$ {{ formatPrice(Number(purchase.value)) }}</span
       >
     </div>
-    <div class="flex flex-col gap-1">
+    <div class="flex flex-col gap-1" v-if="purchase?.type">
       <h2 class="text-lg lg:text-xl">Tipo da compra</h2>
       <span class="text-xl text-zinc-500 lg:text-2xl">
-        {{ convertType("food") }}
+        {{ convertType(purchase.type) }}
       </span>
     </div>
-    <div class="flex flex-col gap-1">
+    <div class="flex flex-col gap-1" v-if="purchase?.description">
       <h2 class="text-lg lg:text-xl">Descrição da compra</h2>
       <p class="text-sm text-zinc-500 lg:text-base">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-        voluptate ipsam ipsa! Sint magni dignissimos unde, nulla corporis earum
-        iusto ipsa vel eius ad nemo tempore perferendis iure quae quod!
+        {{ purchase.description }}
       </p>
     </div>
     <div
+      v-if="purchase"
       class="flex flex-col gap-2 mt-4 md:items-center md:justify-end md:flex-row"
     >
       <button
