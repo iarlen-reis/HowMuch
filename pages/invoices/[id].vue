8475329de0dd68caff3b0dd067148b9ea0f7432d
Iarlen <iarlensr@hotmail.com>
2024-06-11 00:36:22 -0300

    refactor: adding feedback if there is no purchase on the chosen invoice.

--- pages/invoices/[id].vue
+++ pages/invoices/[id].vue
@@ -14,7 +14,7 @@ const chart = await actions.chart.chartById(id);
 </script>
 
 <template>
-  <div class="flex flex-col gap-8" v-if="invoice">
+  <div class="flex flex-col gap-8" v-if="invoice?.data.purchases.length">
     <Navigation
       :title="`Fatura - ${formatTitle(invoice.data.invoice.date)}`"
       url="/invoices"
@@ -36,5 +36,26 @@ const chart = await actions.chart.chartById(id);
         v-bind="item"
       />
     </div>
+  </div>
+
+  <div v-else class="flex flex-col gap-3 items-center justify-center">
+    <img
+      src="/public/images/not-found.png"
+      alt="Nenhuma movimentação registrada"
+      class="w-[300px] h-[300px]"
+    />
+    <h2 class="tracking-wider lg:text-xl text-center">
+      Você ainda não compras
+      <br />
+      para essa fatura
+    </h2>
+
+    <NuxtLink
+      to="/new-entry"
+      class="p-2 flex items-center gap-2 text-center border border-black/40 rounded hover:bg-red-500 hover:text-white hover:border-white/40 transition-colors"
+    >
+      <Icon name="ic:outline-plus" font-size="20" />
+      <span class="text-lg">Nova compra</span>
+    </NuxtLink>
   </div>
 </template>
