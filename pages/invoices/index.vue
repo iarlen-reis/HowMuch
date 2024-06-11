05b04d16c2f70fce133b66218c1bd6702ca6e5de
Iarlen <iarlensr@hotmail.com>
2024-06-11 00:23:35 -0300

    feat: adding feedback if there are no invoices for the next few months.

--- pages/invoices/index.vue
+++ pages/invoices/index.vue
@@ -10,7 +10,7 @@ const data = await actions.invoice.nextInvoices();
 </script>
 
 <template>
-  <div class="flex flex-col gap-5">
+  <div class="flex flex-col gap-5" v-if="data?.data.invoices.length">
     <Navigation url="/" title="Próximas faturas" />
     <div class="flex flex-col mt-4" v-if="data?.data?.total">
       <span>Próximas faturas</span>
@@ -27,5 +27,26 @@ const data = await actions.invoice.nextInvoices();
         :total="Number(invoice.total)"
       />
     </ul>
+  </div>
+  <div v-else class="flex flex-col gap-3 items-center justify-center">
+    <img
+      src="/public/images/not-found.png"
+      alt="Nenhuma movimentação registrada"
+      class="w-[300px] h-[300px]"
+    />
+    <h2 class="tracking-wider lg:text-xl text-center">
+      Você ainda não possui faturas
+      <br />
+      para os proximos
+      <br />
+      meses
+    </h2>
+
+    <NuxtLink
+      to="/"
+      class="p-2 flex items-center gap-2 text-center border border-black/40 rounded hover:bg-red-500 hover:text-white hover:border-white/40 transition-colors"
+    >
+      <span class="text-lg">Página inicial</span>
+    </NuxtLink>
   </div>
 </template>
