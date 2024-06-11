a93de688e8093e021dc10d59d5b9087b019d0717
Iarlen <iarlensr@hotmail.com>
2024-06-11 02:22:16 -0300

    refactor: involving the Avatar component with a nuxt link.

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -12,13 +12,13 @@ defineProps({
 </script>
 
 <template>
-  <img
-    :alt="alt"
-    :src="url"
-    v-if="url"
-    class="w-12 h-12 rounded-xl"
-  />
-  <div v-else class="w-12 h-12 rounded-xl border border-neutral-600/60 flex items-center justify-center">
-    {{ alt?.split(" ")[0].charAt(0) }}{{ alt?.split(" ")[1].charAt(0) }}
-  </div>
+  <NuxtLink to="/profile">
+    <img :alt="alt" :src="url" v-if="url" class="w-12 h-12 rounded-xl" />
+    <div
+      v-else
+      class="w-12 h-12 rounded-xl border border-neutral-600/60 flex items-center justify-center"
+    >
+      {{ alt?.split(" ")[0].charAt(0) }}{{ alt?.split(" ")[1].charAt(0) }}
+    </div>
+  </NuxtLink>
 </template>
