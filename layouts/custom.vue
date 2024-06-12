e8f9a1866f28bdd5f93fd172169acf8e835fc530
Iarlen <iarlensr@hotmail.com>
2024-06-11 23:12:03 -0300

    feat: add toaster on layouts.

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,6 +1,7 @@
 <template>
   <main class="container px-3 mx-auto pb-12 mt-5 min-h-screen">
     <slot />
+    <Toaster position="top-right" richColors />
   </main>
   <Footer />
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <Header />
   <main class="container px-2 mx-auto pb-12 mt-6 min-h-screen">
     <slot />
+    <Toaster position="top-right" richColors />
   </main>
   <Footer />
 </template>
