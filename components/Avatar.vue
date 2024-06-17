ac637861b1a7fd5d2ad77a4b6e04a0fb3be7adf0
Iarlen <iarlensr@hotmail.com>
2024-06-16 22:01:32 -0300

    styles: add animate bounce if there is no profile image.

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -16,7 +16,7 @@ defineProps({
     <img :alt="alt" :src="url" v-if="url" class="w-12 h-12 rounded-xl" />
     <div
       v-else
-      class="w-12 h-12 rounded-xl border border-neutral-600/60 flex items-center justify-center"
+      class="w-12 h-12 rounded-xl border animate-bounce border-neutral-600/60 flex items-center justify-center"
     >
       {{ alt?.split(" ")[0].charAt(0) }}{{ alt?.split(" ")[1]?.charAt(0) }}
     </div>
